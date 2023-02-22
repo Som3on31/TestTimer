dd3b4ad4fb4a10620579b881337db644f97035c9
mintck22 <59277344+mintck22@users.noreply.github.com>
2023-02-22 14:16:25 +0700

    rearrange page

--- app/src/main/java/com/example/testtimer/SummaryFragment.java
+++ app/src/main/java/com/example/testtimer/SummaryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +33,15 @@ public class SummaryFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         super.onViewCreated(view,savedInstanceState);
+
+        binding.btnMenu.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   NavHostFragment.findNavController(SummaryFragment.this)
+                           .navigate(R.id.action_summaryFragment_to_homeFragment);
+               }
+           }
+        );
 
 
     }
