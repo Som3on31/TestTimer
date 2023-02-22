91983ce3da744a67f2fb4ebb852cb5f7ee421df0
mintck22 <59277344+mintck22@users.noreply.github.com>
2023-02-22 12:51:23 +0700

    distance page update

--- app/src/main/java/com/example/testtimer/DistanceFragment.java
+++ app/src/main/java/com/example/testtimer/DistanceFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,7 +35,14 @@ public class DistanceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         super.onViewCreated(view,savedInstanceState);
 
-        // TODO: Displau distance between the user and the device here
+        binding.btnMenu.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   NavHostFragment.findNavController(DistanceFragment.this)
+                           .navigate(R.id.action_distanceFragment_to_homeFragment);
+               }
+           }
+        );
 
     }
 
