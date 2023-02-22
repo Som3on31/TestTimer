f527b0fd4187cf7a47d762e0c577460edacd27d1
mintck22 <59277344+mintck22@users.noreply.github.com>
2023-02-22 15:02:57 +0700

    link home page

--- app/src/main/java/com/example/testtimer/SettingsFragment.java
+++ app/src/main/java/com/example/testtimer/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +33,14 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         super.onViewCreated(view,savedInstanceState);
 
+        binding.btnMenu.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   NavHostFragment.findNavController(SettingsFragment.this)
+                           .navigate(R.id.action_settingsFragment_to_homeFragment);
+               }
+           }
+        );
 
     }
 
