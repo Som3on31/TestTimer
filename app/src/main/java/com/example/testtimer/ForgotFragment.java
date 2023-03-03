dc0560d01ebe0b6ae9dc7e8ed20199f1ea75f5b6
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2023-03-03 23:00:31 +0700

    Linked forgot and register fragments

--- app/src/main/java/com/example/testtimer/ForgotFragment.java
+++ app/src/main/java/com/example/testtimer/ForgotFragment.java
@@ -2,63 +2,48 @@ package com.example.testtimer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForgotFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.testtimer.databinding.FragmentForgotBinding;
+import com.example.testtimer.databinding.FragmentRegisterBinding;
+
+
 public class ForgotFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ForgotFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForgotFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForgotFragment newInstance(String param1, String param2) {
-        ForgotFragment fragment = new ForgotFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    FragmentForgotBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot, container, false);
+        binding = FragmentForgotBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+        //TODO: make it send an email to the des
+        binding.submit.setOnClickListener(View -> {
+            NavHostFragment.findNavController(ForgotFragment.this)
+                    .navigate(R.id.action_forgotFragment_to_loginFragment);
+        });
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
     }
 }
