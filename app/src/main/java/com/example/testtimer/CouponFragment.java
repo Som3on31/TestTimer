4cdd49a2a8a26c3db83187919d58f61b6312cabe
mintck22 <59277344+mintck22@users.noreply.github.com>
2023-03-04 16:39:02 +0700

    show code when clicked redeem

--- app/src/main/java/com/example/testtimer/CouponFragment.java
+++ app/src/main/java/com/example/testtimer/CouponFragment.java
@@ -1,9 +1,12 @@
 package com.example.testtimer;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -32,8 +35,65 @@ public class CouponFragment extends Fragment {
                 NavHostFragment.findNavController(CouponFragment.this)
                         .navigate(R.id.action_couponFragment_to_homeFragment2);
             }
-        }
-        );
+        });
+
+        binding.redeem.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+//                   DialogCouponFragment dialogCouponFragment = new DialogCouponFragment();
+//                   dialogCouponFragment.show(getContext());
+                   AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                   alert.setTitle("Please show this redemption code to the merchant staff.");
+                   alert.setMessage("\n Lkjdef \n");
+                   AlertDialog dialog = alert.show();
+
+                   TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+                   messageView.setGravity(Gravity.CENTER);
+                   messageView.setTextSize(40);
+               }
+           });
+
+        binding.redeem1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Please show this redemption code to the merchant staff.");
+                alert.setMessage("\n Q1Plyk \n");
+                AlertDialog dialog = alert.show();
+
+                TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER);
+                messageView.setTextSize(40);
+            }
+        });
+
+        binding.redeem2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Please show this redemption code to the merchant staff.");
+                alert.setMessage("\n Mko3sr \n");
+                AlertDialog dialog = alert.show();
+
+                TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER);
+                messageView.setTextSize(40);
+            }
+        });
+
+        binding.redeem3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Please show this redemption code to the merchant staff.");
+                alert.setMessage("\n Ipkioj \n");
+                AlertDialog dialog = alert.show();
+
+                TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER);
+                messageView.setTextSize(40);
+            }
+        });
     }
 
     public void onDestroyView() {
