9a92cc6b9bca382d93c8649a0216ccd2a03d398c
mintck22 <59277344+mintck22@users.noreply.github.com>
2023-03-05 16:30:56 +0700

    link login, register

--- app/src/main/java/com/example/testtimer/LoginFragment.java
+++ app/src/main/java/com/example/testtimer/LoginFragment.java
@@ -24,6 +24,8 @@ import com.example.testtimer.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
 
+    FragmentLoginBinding binding;
+
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
     private Button mLoginButton;
@@ -32,19 +34,48 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        // Inflate the layout for this fragment
+        binding =  FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
-        mUsernameEditText = view.findViewById(R.id.login_username);
-        mPasswordEditText = view.findViewById(R.id.login_password);
-        mLoginButton = view.findViewById(R.id.submit);
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+        binding.register2.setOnClickListener(View -> {
+            NavHostFragment.findNavController(LoginFragment.this)
+                    .navigate(R.id.action_loginFragment_to_registerFragment);
+        });
+
+        binding.forgot.setOnClickListener(View -> {
+            NavHostFragment.findNavController(LoginFragment.this)
+                    .navigate(R.id.action_loginFragment_to_forgotFragment);
+        });
+
+//        binding.submit.setOnClickListener(View -> {
+//            // placeholder
+//            boolean passwordCorrect = true;
+//
+//            // logic for managing when the user attempts to log into the app
+//            if(passwordCorrect){
+//                NavHostFragment.findNavController(LoginFragment.this)
+//                        .navigate(R.id.action_loginFragment_to_homeFragment);
+//            }else{
+//
+//            }
+//        });
+
+//        mUsernameEditText = view.findViewById(R.id.login_username);
+//        mPasswordEditText = view.findViewById(R.id.login_password);
+//        mLoginButton = view.findViewById(R.id.submit);
 
         mDatabaseHelper = new DatabaseHelper(getContext());
 
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        binding.submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUsernameEditText.getText().toString().trim();
-                String password = mPasswordEditText.getText().toString().trim();
+                String username = binding.loginUsername.getText().toString().trim();
+                String password = binding.loginPassword.getText().toString().trim();
 
                 if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
                     Toast.makeText(getContext(), "Please enter username and password", Toast.LENGTH_SHORT).show();
@@ -55,14 +86,49 @@ public class LoginFragment extends Fragment {
 
                 if (loginSuccessful) {
                     Toast.makeText(getContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                    NavHostFragment.findNavController(LoginFragment.this)
+                        .navigate(R.id.action_loginFragment_to_homeFragment);
                 } else {
                     Toast.makeText(getContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        return view;
     }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_login, container, false);
+//
+//        mUsernameEditText = view.findViewById(R.id.login_username);
+//        mPasswordEditText = view.findViewById(R.id.login_password);
+//        mLoginButton = view.findViewById(R.id.submit);
+//
+//        mDatabaseHelper = new DatabaseHelper(getContext());
+//
+//        mLoginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String username = mUsernameEditText.getText().toString().trim();
+//                String password = mPasswordEditText.getText().toString().trim();
+//
+//                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+//                    Toast.makeText(getContext(), "Please enter username and password", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//
+//                boolean loginSuccessful = checkLoginCredentials(username, password);
+//
+//                if (loginSuccessful) {
+//                    Toast.makeText(getContext(), "Login successful", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(getContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//
+//        return view;
+//    }
 
     private boolean checkLoginCredentials(String username, String password) {
         SQLiteDatabase db = mDatabaseHelper.getReadableDatabase();
@@ -90,6 +156,8 @@ public class LoginFragment extends Fragment {
 
         return count > 0;
     }
+
+
 
 }
 
--- app/src/main/java/com/example/testtimer/RegisterFragment.java
+++ app/src/main/java/com/example/testtimer/RegisterFragment.java
@@ -27,10 +27,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.testtimer.databinding.FragmentLoginBinding;
 import com.example.testtimer.databinding.FragmentRegisterBinding;
 import com.example.testtimer.databinding.FragmentSettingsBinding;
 
 public class RegisterFragment extends Fragment {
+
+    FragmentRegisterBinding binding;
 
     private EditText firstNameEditText;
     private EditText lastNameEditText;
@@ -48,35 +51,42 @@ public class RegisterFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        binding =  FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
 
 
         // Get references to UI elements
-        firstNameEditText = view.findViewById(R.id.reg_fname);
-        lastNameEditText = view.findViewById(R.id.edit_lname);
-        usernameEditText = view.findViewById(R.id.edit_user);
-        passwordEditText = view.findViewById(R.id.edit_password);
-        emailEditText = view.findViewById(R.id.edit_email);
-        birthdayEditText = view.findViewById(R.id.edit_bd);
-        registerButton = view.findViewById(R.id.register);
-        loginTextView = view.findViewById(R.id.register);
+//        firstNameEditText = view.findViewById(R.id.reg_fname);
+//        lastNameEditText = view.findViewById(R.id.edit_lname);
+//        usernameEditText = view.findViewById(R.id.edit_user);
+//        passwordEditText = view.findViewById(R.id.edit_password);
+//        emailEditText = view.findViewById(R.id.edit_email);
+//        birthdayEditText = view.findViewById(R.id.edit_bd);
+//        registerButton = view.findViewById(R.id.register);
+//        loginTextView = view.findViewById(R.id.register);
 
         // Initialize database
         database = new DatabaseHelper(getActivity()).getWritableDatabase();
 
         // Set click listeners
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        binding.register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Get user input
-                String firstName = firstNameEditText.getText().toString();
-                String lastName = lastNameEditText.getText().toString();
-                String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String email = emailEditText.getText().toString();
-                String birthday = birthdayEditText.getText().toString();
+                String firstName = binding.editFname.getText().toString();
+                String lastName = binding.editLname.getText().toString();
+                String username = binding.editUser.getText().toString();
+                String password = binding.editPassword.getText().toString();
+                String email = binding.editEmail.getText().toString();
+                String birthday = binding.editBd.getText().toString();
 
                 // Insert user input into database
                 ContentValues values = new ContentValues();
@@ -89,31 +99,103 @@ public class RegisterFragment extends Fragment {
                 database.insert(DatabaseHelper.TABLE_USERS, null, values);
 
                 // Clear input fields
-                firstNameEditText.setText("");
-                lastNameEditText.setText("");
-                usernameEditText.setText("");
-                passwordEditText.setText("");
-                emailEditText.setText("");
-                birthdayEditText.setText("");
+                binding.editFname.setText("");
+                binding.editLname.setText("");
+                binding.editUser.setText("");
+                binding.editPassword.setText("");
+                binding.editEmail.setText("");
+                binding.editBd.setText("");
 
                 // Show toast message
                 Toast.makeText(getActivity(), "Registration successful", Toast.LENGTH_SHORT).show();
+
+                //go to loginFragment
+                NavHostFragment.findNavController(RegisterFragment.this)
+                        .navigate(R.id.action_registerFragment_to_loginFragment);
             }
         });
 
-        loginTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Switch to login fragment
-                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_container, new LoginFragment());
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
 
-        return view;
+//        loginTextView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Switch to login fragment
+//                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.fragment_container, new LoginFragment());
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//            }
+//        });
+
     }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_register, container, false);
+//
+//
+//        // Get references to UI elements
+//        firstNameEditText = view.findViewById(R.id.reg_fname);
+//        lastNameEditText = view.findViewById(R.id.edit_lname);
+//        usernameEditText = view.findViewById(R.id.edit_user);
+//        passwordEditText = view.findViewById(R.id.edit_password);
+//        emailEditText = view.findViewById(R.id.edit_email);
+//        birthdayEditText = view.findViewById(R.id.edit_bd);
+//        registerButton = view.findViewById(R.id.register);
+//        loginTextView = view.findViewById(R.id.register);
+//
+//        // Initialize database
+//        database = new DatabaseHelper(getActivity()).getWritableDatabase();
+//
+//        // Set click listeners
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Get user input
+//                String firstName = firstNameEditText.getText().toString();
+//                String lastName = lastNameEditText.getText().toString();
+//                String username = usernameEditText.getText().toString();
+//                String password = passwordEditText.getText().toString();
+//                String email = emailEditText.getText().toString();
+//                String birthday = birthdayEditText.getText().toString();
+//
+//                // Insert user input into database
+//                ContentValues values = new ContentValues();
+//                values.put(DatabaseHelper.COLUMN_FIRST_NAME, firstName);
+//                values.put(DatabaseHelper.COLUMN_LAST_NAME, lastName);
+//                values.put(DatabaseHelper.COLUMN_USERNAME, username);
+//                values.put(DatabaseHelper.COLUMN_PASSWORD, password);
+//                values.put(DatabaseHelper.COLUMN_EMAIL, email);
+//                values.put(DatabaseHelper.COLUMN_BIRTHDAY, birthday);
+//                database.insert(DatabaseHelper.TABLE_USERS, null, values);
+//
+//                // Clear input fields
+//                firstNameEditText.setText("");
+//                lastNameEditText.setText("");
+//                usernameEditText.setText("");
+//                passwordEditText.setText("");
+//                emailEditText.setText("");
+//                birthdayEditText.setText("");
+//
+//                // Show toast message
+//                Toast.makeText(getActivity(), "Registration successful", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        loginTextView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Switch to login fragment
+//                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//                transaction.replace(R.id.fragment_container, new LoginFragment());
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//            }
+//        });
+//
+//        return view;
+//    }
 
     @Override
     public void onDestroy() {
