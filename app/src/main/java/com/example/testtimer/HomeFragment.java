8f9ec7a36ab28131476d5ab621350b31fc3a34f6
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2023-03-11 18:08:27 +0700

    Added alert dialog for logging out of the app

--- app/src/main/java/com/example/testtimer/HomeFragment.java
+++ app/src/main/java/com/example/testtimer/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.testtimer;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -15,6 +20,23 @@ import com.example.testtimer.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    AlertDialog.Builder builder;
+
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        builder = new AlertDialog.Builder(getActivity());
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                createComfirmationPopup();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+    }
 
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -89,8 +111,10 @@ public class HomeFragment extends Fragment {
 
         //login section
         binding.logout.setOnClickListener(View -> {
-            NavHostFragment.findNavController(HomeFragment.this)
-                    .navigate(R.id.action_homeFragment_to_loginFragment);
+            createComfirmationPopup();
+
+//            NavHostFragment.findNavController(HomeFragment.this)
+//                    .navigate(R.id.action_homeFragment_to_loginFragment);
         });
 
         binding.imgBadge.setOnClickListener(new View.OnClickListener()  {
@@ -106,6 +130,36 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    //methods for handling logout
+    public void createComfirmationPopup(){
+        builder.setMessage("Would you like to logout?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Toast.makeText(getContext(),"Successfully logged out",
+                                Toast.LENGTH_SHORT).show();
+
+                        NavHostFragment.findNavController(HomeFragment.this)
+                                .navigate(R.id.action_homeFragment_to_loginFragment);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        dialog.cancel();
+
+                    }
+                });
+
+
+        AlertDialog logoutConfirmationPopup = builder.create();
+        //Set title here
+        logoutConfirmationPopup.setTitle("Confirm Logout?");
+        logoutConfirmationPopup.show();
     }
 
 }
